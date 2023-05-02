4f68df7749ca0cc9801912d1afd494aa8a934d54
validzed <validtry@gmail.com>
2023-05-02 23:39:47 +0700

    Add login feature and custom hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
-/* eslint-disable no-shadow */
-/* eslint-disable react/jsx-one-expression-per-line */
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { Routes, Route } from 'react-router-dom';
 import { asyncPreloadProcess } from './states/isPreload/action';
-import { asyncUnsetAuthUser, asyncSetAuthUser } from './states/authUser/action';
-import { asyncPopulateUsersAndThreads } from './states/shared/action';
-import { asyncAddThread, asyncNeutralizeThreadVote, asyncUpvoteThread } from './states/threads/action';
-import { asyncAddComment, asyncReceiveThreadDetail } from './states/threadDetail/action';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   const firstRun = React.useRef(true);
@@ -14,36 +10,13 @@ function App() {
   const {
     authUser = null,
     isPreload = false,
-    threads = [],
-    users = [],
-    threadDetail = [],
   } = useSelector((state) => state);
 
   const dispatch = useDispatch();
 
-  // Testing
-  const user = {
-    email: 'admin111@gmail.com',
-    password: 'admin111',
-  };
-
-  const threadPayload = {
-    title: 'Cara cepat kaya',
-    body: 'ada 100 cara cepat kaya',
-    category: 'finance',
-  };
-
-  const threadID = 'thread-08_nUU2fhu1P5nre';
-
-  const komentar = 'test komen 001';
-  // End Test
-
   React.useEffect(() => {
     if (firstRun.current) {
-      dispatch(asyncSetAuthUser(user));
       dispatch(asyncPreloadProcess());
-      dispatch(asyncPopulateUsersAndThreads());
-      dispatch(asyncReceiveThreadDetail(threadID));
       firstRun.current = false;
     }
   }, [dispatch]);
@@ -64,80 +37,20 @@ function App() {
     });
   };
 
-  const threadList = threads.map((thread) => ({
-    ...thread,
-    creator: users.find((user) => user.id === thread.ownerId),
-    authUser: authUser.id,
-  }));
+  if (isPreload) {
+    return null;
+  }
+
+  if (authUser === null) {
+    return (
+      <Routes>
+        <Route path="/*" element={<LoginPage />} />
+      </Routes>
+    );
+  }
 
   return (
-    <div className="dark:bg-slate-800 bg-white">
-      {/* {console.log('threads: ', threads)}
-      {console.log('users: ', users)}
-      {console.log('thread list: ', threadList)}
-      {console.log('thread detail:', threadDetail)} */}
-      <header>
-        <img className="h-20 w-auto" src="./icons/logo.svg" alt="Logo" />
-        <p className="text-2xl dark:text-white">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button className="bg-sky-900 text-white mt-3" type="button" onClick={onChangeTheme}>
-          Change Theme
-        </button>
-      </header>
-      <main>
-        <h2 className="mt-3 text-3xl text-slate-900 dark:text-slate-100">Title Discussion</h2>
-        <p className="text-xl font-light text-slate-900 dark:text-slate-100">
-          Tailwind CSS is a utility-first CSS framework for rapid UI development created by Adam
-          Wathan, Jonathan Reinink, David Hemphill and Steve Schoger.
-        </p>
-        <div>
-          <button
-            className="dark:text-green-300"
-            type="button"
-            onClick={() => dispatch(asyncAddThread(threadPayload))}
-          >
-            Tambah Thread
-          </button>
-        </div>
-        <div>
-          <button
-            className="dark:text-sky-300"
-            type="button"
-            onClick={() => dispatch(asyncAddComment(threadID, komentar))}
-          >
-            Tambah Comment
-          </button>
-        </div>
-        <div>
-          <button
-            className="dark:text-red-500"
-            type="button"
-            onClick={() => dispatch(asyncUpvoteThread(threadID))}
-          >
-            Like Thread
-          </button>
-        </div>
-        <div>
-          <button
-            className="dark:text-red-500"
-            type="button"
-            onClick={() => dispatch(asyncNeutralizeThreadVote(threadID))}
-          >
-            Neutralize Thread Vote
-          </button>
-        </div>
-        <div>
-          <button
-            className="dark:text-yellow-300"
-            type="button"
-            onClick={() => dispatch(asyncUnsetAuthUser())}
-          >
-            Log out
-          </button>
-        </div>
-      </main>
-    </div>
+    <h1>HomePage</h1>
   );
 }
 
